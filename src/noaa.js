9fa11fac32bd8295c799158a18f412222cb0ff0e
Abdul Osman <39681296+osmaniacattack@users.noreply.github.com>
2024-12-15 16:07:15 -0500

    Remove unused files

--- src/noaa.js
+++
@@ -1,9 +0,0 @@
-const noaa = {
-    'VA': {
-        'Greene County': 'VAC079',
-        'Greensville County': 'VAC081',
-        'Halifax County': 'VAC083',
-        'Hanover County': 'VAC085',
-        'Henrico County': 'VAC087'
-    }
-}
